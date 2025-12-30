ecf899af24d5f2aacd0b9a3e282fdf8e0cc77e31
Joao Coelho <joaonc@users.noreply.github.com>
2025-12-29 21:57:38 -0600

    Static files fixes (#30)

--- users/users/admin.py
+++ users/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from .models import User
 
+# Use custom user model only; hide Django's built-in Group model (not used)
+admin.site.unregister(Group)
 admin.site.register(User)
